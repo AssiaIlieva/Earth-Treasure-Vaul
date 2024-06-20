507c66e5809b45be369bd316faaa1cd4266cbd42
AssiaIlieva <ac17@abv.bg>
2024-06-20 19:11:41 +0300

    Update the DB name and User Model

--- src/models/User.js
+++ src/models/User.js
@@ -2,17 +2,15 @@ const {Schema, model, MongooseError} = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
-    username: {
+      email: {
         type: String,
-        required: [true, 'Username is required'],
-    },
-    email: {
-        type: String,
+        minLength: 10,
         required: [true, 'Email is required'],
         unique: true,
     },
     password: {
         type: String,
+        minLength: 4,
         required: [true, 'Password is required'],
     }
 });
